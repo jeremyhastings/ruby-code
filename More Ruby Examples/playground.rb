e59c220ae08b4e8c3b80c0ccdc7c2961f35bf2e6
Jeremy Hastings <jeremy.hastings@mac.com>
2020-03-16 09:59:59 +0000

    Introduction to Section 2 and Ruby Completed

---
+++ More Ruby Examples/playground.rb
@@ -0,0 +1,13 @@
+puts "Hello World"
+print "Hello World\n"
+p "Hello World"
+pp ["Hello", "World"]
+
+greeting = "Hello World"
+puts greeting
+
+def say_hello(thing_to_say)
+  puts thing_to_say
+end
+
+say_hello "Hello Jeremy"
